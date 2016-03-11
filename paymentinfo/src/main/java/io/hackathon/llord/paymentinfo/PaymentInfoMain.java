1471167e62e18fa5cbd03cc2b9b6d5da8499b2b6
Jaime Clinton <jaime.clinton3@gmail.com>
2016-03-11 21:44:15 +0000

    Initial commit.

---
+++ paymentinfo/src/main/java/io/hackathon/llord/paymentinfo/PaymentInfoMain.java
@@ -0,0 +1,12 @@
+package io.hackathon.llord.paymentinfo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentInfoMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
