b6f91eb8a643dfc303c2a931307db3d53cb3f941
James Butler <jamesbutler28@googlemail.com>
2016-03-12 02:04:36 +0000

    Update for broken type

--- paymentinfo/src/main/java/io/hackathon/llord/paymentinfo/impl/PaymentInfoImpl.java
+++ paymentinfo/src/main/java/io/hackathon/llord/paymentinfo/impl/PaymentInfoImpl.java
@@ -24,12 +24,12 @@ public class PaymentInfoImpl {
 	public void firstTimeSetup() {
 		paymentRepository.deleteAll();
 		Payment first = new Payment("Jaime", "Clinton", "40 Magic Road, MAG 1CS", "Washing Machine", "Requested",
-				132.99, "GBP", LocalDateTime.now().minusHours(5).minusMinutes(22));
+				13299, "GBP", LocalDateTime.now().minusHours(5).minusMinutes(22));
 		Payment second = new Payment("James", "Butler", "420 High Street, H14 1EL", "Plumber payment", "Requested",
-				623.00, "GBP", LocalDateTime.now().minusDays(22).minusHours(12).minusMinutes(3));
-		Payment third = new Payment("Quynh", "Nhim", "12 Frontside, E12 3FA", "Hair Dryer", "Requested", 12.99, "GBP",
+				62300, "GBP", LocalDateTime.now().minusDays(22).minusHours(12).minusMinutes(3));
+		Payment third = new Payment("Quynh", "Nhim", "12 Frontside, E12 3FA", "Hair Dryer", "Requested", 1299, "GBP",
 				LocalDateTime.now().minusDays(2).minusHours(7).minusMinutes(50));
-		Payment fourth = new Payment("Saci", "Sebo", "12 Frontside, E12 3FA", "TV", "Requested", 975.00, "GBP",
+		Payment fourth = new Payment("Saci", "Sebo", "12 Frontside, E12 3FA", "TV", "Requested", 97500, "GBP",
 				LocalDateTime.now().minusDays(7).minusHours(3).minusMinutes(11));
 
 		paymentRepository.save(first);
